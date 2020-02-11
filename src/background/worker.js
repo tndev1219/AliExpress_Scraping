60fe727735a1da2195322eebcc8c76f13cba7b59
tndev1219 <53935661+tndev1219@users.noreply.github.com>
2020-02-12 05:46:14 +0800

    mapping language with sources table

--- src/background/worker.js
+++ src/background/worker.js
@@ -10,14 +10,14 @@ const aliExpressWorker = (product) => {
     db.query(Source.getSourceByFieldNameSQL('store_language'), [product.language], (err, data) => {
         if (!err) {
             if (data && data.length > 0) {
-                let domain = data[0].store_url
+                let domain = data[0].store_url;
                 let params = {
                     uuid: uuidv4(),
                     num_products: 1,
                     created_at: moment(Date.now()).format("YYYY-MM-DD hh:mm:ss"),
                     updated_at: moment(Date.now()).format("YYYY-MM-DD hh:mm:ss")
-                }
-                let aliRequest = new AliRequest()
+                };
+                let aliRequest = new AliRequest();
                 db.query(aliRequest.getAddAliRequestSQL(), params, (err, data) => {
                     let params = {
                         uuid: uuidv4(),
@@ -28,16 +28,11 @@ const aliExpressWorker = (product) => {
                         imported: 0,
                         created_at: moment(Date.now()).format("YYYY-MM-DD hh:mm:ss"),
                         updated_at: moment(Date.now()).format("YYYY-MM-DD hh:mm:ss")
-                    }
+                    };
                     let aliQueue = new AliQueue();
                     db.query(aliQueue.getAddAliQueueSQL(), params, (err, data) => {
                         if (!err) {
                             let startUrl = domain + 'item/' + product.code + '.html';
-                            // db.query(AliQueue.getAliQueueByFieldNameSQL('status'), ['READY'], (err, data) => {
-                            //     if (!err) {
-                            //         if (data && data.length > 0) {
-                            //             data.map((d, key) => {
-                            //                 if (d.product_code === product.code.toString()) {                                            
                             let params = [
                                 'RESERVED',
                                 moment(Date.now()).format("YYYY-MM-DD hh:mm:ss"),
@@ -50,11 +45,6 @@ const aliExpressWorker = (product) => {
                             db.query(AliQueue.updateAliQueueByFieldNameSQL(fields, condition), params, async (err, data) => {
                                 await callApifyMain(startUrl);
                             });
-                            //                 }
-                            //             });
-                            //         }
-                            //     }
-                            // });
                         }
                     });
                 });
