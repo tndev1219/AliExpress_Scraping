1403ed3ad99b2ec84fe3ac70283e74f6d55c053e
tndev1219 <53935661+tndev1219@users.noreply.github.com>
2020-02-12 08:18:27 +0800

    user authontication with Bearer token
    requirement 6

--- src/api/sources.js
+++ src/api/sources.js
@@ -2,6 +2,8 @@ var express = require('express');
 var Queue = require('bull');
 require('dotenv').config()
 const router = express.Router();
+var db = require('../db/database');
+var User = require('../domain/user');
 
 const workQueue = new Queue('worker', {
     redis: {
@@ -10,34 +12,60 @@ const workQueue = new Queue('worker', {
     }
 });
 
-router.post("/products", async (req, res, next) => {
-    let products = [];
-    if (req.body.products) {
-        products = req.body.products;
-    } else if (req.body.items && req.body.languages) {
-        req.body.items.map((item1) => {
-            req.body.languages.map((item2) => {
-                var product = {};
-                product.code = item1;
-                product.language = item2;
-                products.push(product);
-            })
-        });
-
-    } else {
-        return res.json({ message: 'Invalid Payload' });
-    }
-
-    products.map(async (product, key) => {
-        const data = { product };
-        const options = {
-            delay: 20,
-            attempts: 3
+const authCheck = (token, callback) => {
+    db.query(User.getUserByFieldNameSQL('api_token'), [token], (err, data) => {
+        if (err) {
+            callback(err.message, null);
+        } else {
+            if (data && data.length > 0) {
+                callback(null, data);
+            } else {
+                callback('Authontication failed!', null);
+            }
         }
-        await workQueue.add(data, options);
-    });
-    
-    res.json({ message: "ok" });
+    })
+}
+
+router.post("/products", async (req, res, next) => {
+
+    if (req.headers.authorization) {
+        authCheck(req.headers.authorization.split(' ')[1], (err, data) => {
+            if (err) {
+                return res.json({ message: err });
+            } else {
+                let products = [];
+
+                if (req.body.products) {
+                    products = req.body.products;
+                } else if (req.body.items && req.body.languages) {
+                    req.body.items.map((item1) => {
+                        req.body.languages.map((item2) => {
+                            var product = {};
+                            product.code = item1;
+                            product.language = item2;
+                            products.push(product);
+                        })
+                    });
+            
+                } else {
+                    return res.json({ message: 'Invalid Payload' });
+                }
+            
+                products.map(async (product, key) => {
+                    const data = { product };
+                    const options = {
+                        delay: 20,
+                        attempts: 3
+                    }
+                    await workQueue.add(data, options);
+                });
+            
+                return res.json({ message: "ok" });                
+            }
+        })
+    } else {
+        return res.json({ message: 'Authontication failed!' })
+    }
 })
 
 workQueue.on('global:completed', (jobId, result) => {
---
+++ src/domain/user.js
@@ -0,0 +1,38 @@
+class User {
+
+    constructor(){
+
+    }
+
+    getAddUserSQL() {
+        let sql = `INSERT INTO users SET ?`
+        return sql;
+    }
+
+    static getUserByIdSQL() {
+        let sql = `SELECT * FROM users WHERE id=?`;
+        return sql;
+    }
+
+    static getUserByFieldNameSQL(fieldName) {
+        let sql = `SELECT * FROM users WHERE ${fieldName}=?`
+        return sql;
+    }
+
+    static updateUserByFieldNameSQL(fields, condition){
+        let sql = `UPDATE users SET ${fields} WHERE ${condition}`
+        return sql
+    }
+
+    static deleteUserByIdSQL() {
+        let sql = `DELETE FROM users WHERE id=?`;
+        return sql;
+    }
+
+    static getAllUserSQL() {
+        let sql = `SELECT * FROM users`;
+        return sql;
+    }    
+}
+
+module.exports = User;
